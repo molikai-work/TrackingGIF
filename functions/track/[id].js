f4c9f3488fca93cb50e5f1081f3c9ff0f564530e
molikai-work <165390418+molikai-work@users.noreply.github.com>
2025-04-05 12:53:32 +0800

    优化 track/[id].js 注释

--- functions/track/[id].js
+++ functions/track/[id].js
@@ -41,7 +41,7 @@ export async function onRequest(context) {
     }
     const trackingId = rawId.slice(0, -4); // 去除 .gif 后缀
 
-    // 获取访客 IP、地区和 UA
+    // 获取访客的 IP、地区和 UA
     const clientIP = request.headers.get("CF-Connecting-IP");
     const country = request.headers.get("CF-IPCountry");
     const userAgent = request.headers.get("User-Agent") || null;
